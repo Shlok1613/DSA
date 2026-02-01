e100c2f9f64a947e3c0d868ba92b67ae37e9543f
Shlok1613 <shlokshah16042004@gmail.com>
2026-02-01 15:17:26 +0530

    Satck 234

--- Recursion/palindrome_linked_list.py
+++ Recursion/palindrome_linked_list.py
@@ -28,27 +28,3 @@ class Solution:
             return is_equal
         
         return rec(head)
-    
-############################# OR #############################
-
-#Pattern Used: Stack
-#Time Complexity: O(n)
-#Space Complexity: O(n)
-#Short Explanation: We push all the values of the linked list onto a stack,
-#                   then compare the values popped from the stack with the original list values.
-
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-
-        while head:
-            stack.append(head.val)
-            head = head.next
-        
-        return stack == stack[::-1]
