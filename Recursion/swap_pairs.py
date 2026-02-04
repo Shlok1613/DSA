d748d19f32b67378acf9bdfeffc5f57d41f65c8e
Shlok1613 <shlokshah16042004@gmail.com>
2026-02-04 18:16:33 +0530

    Recursion 24

---
+++ Recursion/swap_pairs.py
@@ -0,0 +1,23 @@
+# Problem Name: Swap Nodes in Pairs
+# Pattern Used: Recursion
+# Time Complexity: O(n)
+# Space Complexity: O(n) due to recursion stack
+# Short Explanation: We recursively swap pairs of nodes by adjusting their next pointers.
+# LeetCode: https://leetcode.com/problems/swap-nodes-in-pairs/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        first = head
+        second = head.next
+        first.next = self.swapPairs(second.next)
+        second.next = first
+
+        return second
