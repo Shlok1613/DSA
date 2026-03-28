c8414908048a3e3919db2562074871e2ed5af4cf
Shlok1613 <shlokshah16042004@gmail.com>
2026-03-28 19:48:57 +0530

    Recover Binary Search Tree 99

---
+++ Binary Tree/recover_binary_search_tree.py
@@ -0,0 +1,36 @@
+# Problem Name: Recover Binary Search Tree
+# Pattern Used: Recursion
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+# Short Explanation: We use recursion to find the two nodes that are swapped in a binary search tree. 
+#                    We first check if the current node is the root or one of the target nodes, if so, we return it. 
+#                    Then we recursively check the left and right subtrees. 
+#                    If both subtrees return a non-null value, it means the current node is the lowest common ancestor. 
+#                    Otherwise, we return the non-null value from either the left or right subtree.
+# LeetCode Link: https://leetcode.com/problems/recover-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        self.prev = self.first = self.second = None
+
+        def validate(node):
+            if not node:
+                return
+
+            validate(node.left)
+            if self.prev and node.val < self.prev.val:
+                if not self.first:
+                    self.first = self.prev
+                self.second = node
+            self.prev = node
+            validate(node.right)
+
+        validate(root)
+        
+        self.first.val, self.second.val = self.second.val, self.first.val
