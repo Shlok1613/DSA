742a0b5aeaf39e9b815e0d6d48f7b4cf0c81e08f
Shlok1613 <shlokshah16042004@gmail.com>
2026-07-09 12:19:00 +0530

    Single Number

---
+++ Hash Table/single_number.py
@@ -0,0 +1,19 @@
+# Problem Name: Single Number
+# Pattern Used: Hash Table
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Short Explanation: We use a hash table to count occurrences of each number and return the one that occurs only once.
+# LeetCode: https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = {}
+        ans = None
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        for i in count:
+            if count[i] == 1:
+                ans = i
+
+        return ans 
