bd9cc230071e56841d9c86c197732f8f58c659cb
Shlok1613 <shlokshah16042004@gmail.com>
2026-07-15 15:44:06 +0530

    3658 GCD of Odd and Even Sums

---
+++ Math/GCD_of_odd_and_even_sums.py
@@ -0,0 +1,10 @@
+# Problem Name: GCD of Odd and Even Sums
+# Pattern Used: Math
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+# Short Explanation: We use math to find the GCD of odd and even sums.
+# LeetCode Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
