9d5fc0a1f8b5d6bbcc9a0a9386b298d4b809ea99
Shlok1613 <shlokshah16042004@gmail.com>
2026-09-02 21:44:21 +0530

    3875. Construct Uniform Parity Array I

---
+++ Array/construct_uniform_parity_array_I.py
@@ -0,0 +1,10 @@
+# Problem Name: Construct Uniform Parity Array I
+# Pattern Used: Array Manipulation
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+# Short Explanation: The function checks if the given array can be transformed into a uniform parity array
+# LeetCode Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
